f0213b0dd82ecffd8cdbad1681b6f404674eea8d
Nikhil-M-C-B <chowdary.boyapati@students.iiit.ac.in>
2025-09-10 15:50:18 +0530

    Delete shell directory

--- shell/include/input.h
+++
@@ -1,6 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-char *read_input(void);
-
-#endif
--- shell/include/prompt.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROMPT_H
-#define PROMPT_H
-
-char *build_prompt(const char *shell_home);
-void free_prompt(char *p);
-void show_prompt(void);
-
-#endif
